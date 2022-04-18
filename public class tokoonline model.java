488181752643039ad07869679a75dab7486468c9
dimasnfa <103722609+dimasnfa@users.noreply.github.com>
2022-04-18 16:02:27 +0700

    Add files via upload

---
+++ public class tokoonline model.java
@@ -0,0 +1,16 @@
+public class tokoonline {
+    private String namabarang;
+    private String jenisbarang;
+    private int hargabarang;
+    private String deskripsibarsng;
+    private String asalbarang;
+    private String statusstock;
+
+    public tokoonline(String namabarang,String jenisbarang,int hargabarang,String deskripsibarsng,String asalbarang,String statusstock)(
+        this.namabarang = namabarang;
+        this.jenisbarang = jenisbarang;
+        this.hargabarang = hargabarang;
+        this.deskripsibarsng = deskripsibarsng;
+        this.asalbarang = asalbarang;
+        this.statusstock = statusstock;
+}
